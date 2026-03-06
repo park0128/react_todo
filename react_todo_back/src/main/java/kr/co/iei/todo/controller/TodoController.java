870232501adec614720b60af50e5e4bf91cf8374
park0128 <pjy2128@naver.com>
2026-03-06 10:36:13 +0900

    Initial commit

---
+++ react_todo_back/src/main/java/kr/co/iei/todo/controller/TodoController.java
@@ -0,0 +1,18 @@
+package kr.co.iei.todo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kr.co.iei.todo.model.service.TodoService;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+	@Autowired
+	private TodoService todoService;
+	
+	
+}
